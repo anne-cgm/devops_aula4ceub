b1f7e94cdabc2c476a26b5e3a26d9698dbba6255
Anne Caroline Gonçalves de Mesquita <anniie@dinfor.uniceub>
2026-09-03 09:40:26 -0300

    fix: alteração do nome da api_key e adição do .gitleaks

--- src/calculadora.js
+++ src/calculadora.js
@@ -12,4 +12,4 @@ module.exports = {
 };
 
 //Simulação de vazamento de segredo
-const AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE1234567890abcdef";
+const api_key = "AKIAIOSFODNN7EXAMPLE1234567890abcdef";
